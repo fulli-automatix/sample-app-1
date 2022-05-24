b9388c759fde59bf0a45ab62ffe8479eeec47d33
fulli-automatix <95256100+fulli-automatix@users.noreply.github.com>
2022-05-24 12:02:31 +0100

    Update pipeline.groovy

--- cicd/pipeline.groovy
+++ cicd/pipeline.groovy
@@ -1,4 +1,7 @@
 pipelineJob('microservice-0001') {
+  // logRotator(int daysToKeep = -1, int numToKeep = -1, int artifactDaysToKeep = -1, int artifactNumToKeep = -1)
+  logRotator(-1,5,-1,-1)
+    
 
 }
   
